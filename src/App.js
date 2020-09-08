9de6b4b7392a77eb1b8c691eda66746e7f5d97b1
Sudarshan K J <kjsudi@gmail.com>
2020-09-08 09:05:17 +0530

    Remove unnecessary usage of useEffect, instead update using local var

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import styles from "./App.module.css";
 import { getLanguageObject } from "./components/Language";
 import i18next from "i18next";
-//import useSkipFirstRender from "./hooks/useSkipFirstRender";
 
 const App = () => {
   const [language, setLanguage] = useState(i18next.language);
-  const [, setUpdate] = useState(false);
 
   const onToggleLanguage = () => {
     let toggledLanguage = getLanguageObject(language, true).value;
-    setLanguage(toggledLanguage);
+    i18next.changeLanguage(toggledLanguage);
+    setLanguage(toggledLanguage); // This ensures re-render
   };
-
-  //Adding this dependency 'language' is very important. Otherwise it will cause infinite renders.
-  useEffect(() => {
-    i18next.changeLanguage(language);
-    setUpdate((prevupdate) => !prevupdate);
-  }, [language]);
 
   return (
     <div className={styles.wrapper}>
