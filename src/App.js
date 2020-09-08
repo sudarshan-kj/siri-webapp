19ed3628f3b29cea4730e322dfa80518686d91e2
Sudarshan K J <kjsudi@gmail.com>
2020-09-08 08:59:33 +0530

    Setting dummystate in App so that force render can be initiated in useEffect()

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import styles from "./App.module.css";
 import { getLanguageObject } from "./components/Language";
 import i18next from "i18next";
+//import useSkipFirstRender from "./hooks/useSkipFirstRender";
 
 const App = () => {
-  const [language, setLanguage] = React.useState(i18next.language);
+  const [language, setLanguage] = useState(i18next.language);
+  const [, setUpdate] = useState(false);
+
   const onToggleLanguage = () => {
     let toggledLanguage = getLanguageObject(language, true).value;
-    i18next.changeLanguage(toggledLanguage);
     setLanguage(toggledLanguage);
   };
+
+  //Adding this dependency 'language' is very important. Otherwise it will cause infinite renders.
+  useEffect(() => {
+    i18next.changeLanguage(language);
+    setUpdate((prevupdate) => !prevupdate);
+  }, [language]);
 
   return (
     <div className={styles.wrapper}>
