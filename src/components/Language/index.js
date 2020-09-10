bc438456fe5d9084f7893af43b82ff8f293b790c
Sudarshan K J <kjsudi@gmail.com>
2020-09-10 16:45:53 +0530

    1. Added logo support for both languages :: 2. Added a new function to check if language is valid :: 3. Added a initial default value for getLanguageObject() method :: 4. Edited translations

--- src/components/Language/index.js
+++ src/components/Language/index.js
@@ -10,13 +10,17 @@ export const Lang = {
   KAN: { value: "kan", displayName: " ಕನ್ನಡ" },
 };
 
-export const getLanguageObject = (lang, toggle) => {
-  if (lang) {
+export const getLanguageObject = (lang, toggle = false) => {
+  if (isValidLanguage(lang)) {
     if (toggle) {
       return lang === Lang.ENG.value ? Lang.KAN : Lang.ENG;
     }
     return lang === Lang.ENG.value ? Lang.ENG : Lang.KAN;
   }
+};
+
+const isValidLanguage = (lang) => {
+  return Object.keys(Lang).some((key) => Lang[key]["value"] === lang);
 };
 
 const LanguageButton = ({ onToggleLanguage }) => {
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "./Logo.module.css";
-import LogoIcon from "assets/logo-eng.png";
-const Logo = () => (
-  <div className={styles.logoContainer}>
-    <img className={styles.logo} src={LogoIcon} alt="Logo" />
-  </div>
-);
+import { Lang, getLanguageObject } from "components/Language";
+import LogoIconEng from "assets/logo-eng.png";
+import LogoIconKan from "assets/logo-kan.png";
+import i18next from "i18next";
+import i18n from "utils/i18n";
+
+const Logo = () => {
+  const language = getLanguageObject(i18next.language);
+  const langLogo =
+    language === Lang.KAN ? (
+      <img src={LogoIconKan} alt={i18n.t("Siri Logo")} />
+    ) : (
+      <img src={LogoIconEng} alt={i18n.t("Siri Logo")} />
+    );
+
+  return <div className={styles.logoContainer}>{langLogo}</div>;
+};
 
 export default Logo;
