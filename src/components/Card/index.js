ec7d66b06aba9d18a24eda519a22a4f5d535b886
Sudarshan K J <kjsudi@gmail.com>
2020-09-21 20:25:50 +0530

    Cleanup and added title for iframe since webpack was complaining

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "./index.module.css";
-import SlideShow from "components/SlideShow";
-import { Element } from "react-scroll";
 
 const Card = ({ style, size, children }) => {
   return (
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -84,6 +84,7 @@ const Main = () => {
         <div className={styles.cardContainer}>
           <Card style={{ marginTop: "5rem" }} size="xl">
             <iframe
+              title="G Maps Siri Location"
               className={styles.frame}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7775.3037460087935!2d75.3139384!3d12.994104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3830e430a3619d9c!2sSIRI%20Dharmasthala%20Gramodhyoga%20Samsthe!5e0!3m2!1sen!2sin!4v1600696755942!5m2!1sen!2sin"
               frameborder="0"
