59f60365722484f6ecc25556b928ad93d19807b6
Sudarshan K J <kjsudi@gmail.com>
2020-09-21 17:28:17 +0530

    Made the dropdown for mobile completely aesthetic. One of the best feelings today. Cleverest part was making 2 divs, which will register an outside click and close the modal automatically

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,6 +4,47 @@ import { ReactComponent as MenuIcon } from "assets/dropdown.svg";
 import { Link } from "react-scroll";
 
 const Navigation = ({ list }) => {
+  const [dropDownVisibility, setDropDownVisibility] = React.useState("none");
+  const [dropDownIconOrientation, setDropDownIconOrientation] = React.useState({
+    transform: "rotate(0deg)",
+  });
+
+  const toggleDropDown = () => {
+    setDropDownVisibility((prev) => (prev === "flex" ? "none" : "flex"));
+  };
+
+  React.useEffect(() => {
+    if (dropDownVisibility === "none") {
+      setDropDownIconOrientation({
+        transform: "rotate(0deg)",
+      });
+    } else {
+      setDropDownIconOrientation({
+        transform: "rotate(180deg)",
+      });
+    }
+  }, [dropDownVisibility]);
+
+  const outsideClickRef = React.useRef();
+
+  React.useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        outsideClickRef.current &&
+        !outsideClickRef.current.contains(event.target)
+      ) {
+        setDropDownVisibility("none");
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [outsideClickRef]);
+
+  /*Having 2 outer divs for dropdown has automatically made it close the modal once you click elsewhere since the click on links is outside the div*/
+
   return (
     <nav className={styles.nav}>
       <ul>
@@ -20,13 +61,27 @@ const Navigation = ({ list }) => {
           </li>
         ))}
       </ul>
-      <div className={styles.dropdown}>
-        <MenuIcon className={styles.dropbtn} />
-        <div className={styles.dropdownContent}>
+      <div>
+        <div ref={outsideClickRef} className={styles.dropdown}>
+          <MenuIcon
+            style={dropDownIconOrientation}
+            className={styles.dropbtn}
+            onClick={toggleDropDown}
+          />
+        </div>
+        <div
+          style={{ display: dropDownVisibility }}
+          className={styles.dropdownContent}
+        >
           {list.map((item, id) => (
-            <a rel="noopener noreferrer" href={item.link}>
+            <Link
+              activeClass="active"
+              to={item.link}
+              smooth={true}
+              duration={500}
+            >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
       </div>
