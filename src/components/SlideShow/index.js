df2c39b5aede6f0edc98d0a14b790048c67bb665
Sudarshan K J <kjsudi@gmail.com>
2020-11-03 20:10:10 +0530

    Added index.js to include so that multiple images can be looped through easily

--- src/components/SlideShow/index.js
+++ src/components/SlideShow/index.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
-import green from "assets/slides/green.jpg";
-import girl from "assets/slides/girl.jpg";
-import bulb from "assets/slides/bulb.jpg";
+import images from "assets/slides";
 import { css } from "@emotion/core";
 import PulseLoader from "react-spinners/PulseLoader";
 import styles from "./index.module.css";
@@ -56,33 +54,20 @@ const Slideshow = () => {
         duration={2000}
         transitionDuration={500}
       >
-        <div className={styles.eachSlide}>
-          <div className={styles.bgContainer}>
-            <img src={green} alt="fornow" onLoad={imageLoaded} />
-            <span className={styles.tint}>
-              <p>This is Sudarshan KJ and he is going to be the best ever!</p>
-            </span>
-          </div>
-        </div>
-
-        <div className={styles.eachSlide}>
-          <div className={styles.bgContainer}>
-            <img src={girl} alt="fornow" />
-            <span className={styles.tint}>
-              <p>This is Sudarshan KJ and he is going to be the best ever!</p>
-            </span>
-            {/*Refer to corresponding CSS for the reasoning <span /> */}
-          </div>
-        </div>
-        <div className={styles.eachSlide}>
-          <div className={styles.bgContainer}>
-            <img src={bulb} alt="fornow" />
-            <span className={styles.tint}>
-              <p>This is Sudarshan KJ and he is going to be the best ever!</p>
-            </span>
-            {/*Refer to corresponding CSS for the reasoning <span /> */}
-          </div>
-        </div>
+        {images.map((image) => {
+          return (
+            <div className={styles.eachSlide}>
+              <div className={styles.bgContainer}>
+                <img src={image} alt="fornow" onLoad={imageLoaded} />
+                <span className={styles.tint}>
+                  <p>
+                    This is Sudarshan KJ and he is going to be the best ever!
+                  </p>
+                </span>
+              </div>
+            </div>
+          );
+        })}
       </Slide>
     </div>
   );
