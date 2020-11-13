766f2133a3106e20c7d3425109a04b5993f489d4
Sudarshan K J <kjsudi@gmail.com>
2020-11-13 16:52:29 +0530

    Updated icons

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,7 +6,13 @@ import drVeeren from "assets/avatars/drVeerendraHeggade.jpg";
 import hemavathiHeggade from "assets/avatars/hemavathiheggade.jpg";
 import ContactUs from "components/ContactUs";
 import { Link } from "react-router-dom";
+import { Trans } from "react-i18next";
+import { ReactComponent as FranchiseIcon } from "assets/icons/categories/franchise.svg";
+import { ReactComponent as PlayIcon } from "assets/icons/common/play-button.svg";
+import { ReactComponent as CatalogIcon } from "assets/icons/common/catalog.svg";
+import { ReactComponent as ContactUsIcon } from "assets/icons/common/contact-us.svg";
 import useAutoScroll from "hooks/useAutoScroll";
+import useSlideIn from "hooks/useSlideIn";
 
 const Main = () => {
   const [loading, setLoading] = React.useState(true);
@@ -14,34 +20,68 @@ const Main = () => {
     setLoading(false);
   };
 
+  const [founderRef, slideIn] = useSlideIn();
   useAutoScroll();
 
   return (
     <div className={styles.main}>
       <div className={styles.introContainer}>
-        <h1>{i18n.t("Introduction")}</h1>
+        <h1>{i18n.t("intro_heading")}</h1>
+        <p>
+          <Trans i18nKey="content_para_1">
+            <strong />
+            <i />
+          </Trans>
+        </p>
       </div>
       <div>
         <article>
           <section>
-            <Link to="/#video">Click to watch a short video</Link>
-
-            <h3>Section 1</h3>
-            <p>This is section 1 paragraph</p>
+            <p>
+              <Trans i18nKey="content_para_2">
+                <strong />
+                <i />
+              </Trans>
+            </p>
           </section>
-          <section>
-            <h3>Section 2</h3>
-            <p>This is section 2 paragraph</p>
-          </section>
+          <Link to="/#video">
+            <PlayIcon className={styles.linkIcon} />
+            {i18n.t("watch_video")}
+          </Link>
+          <Link to="/products">
+            <CatalogIcon className={styles.linkIcon} />
+            {i18n.t("check_product_range")}
+          </Link>
         </article>
-        <aside>Quick links come here</aside>
+        <div className={styles.horizontalDivider}>
+          <hr />
+        </div>
+        <section>
+          <div className={` ${styles.iconContainer}`}>
+            <FranchiseIcon className={styles.franchiseIcon} />
+            <h3>Franchise Call Out</h3>
+          </div>
+          <p>
+            <Trans i18nKey="content_para_3">
+              <strong />
+              <i />
+            </Trans>
+          </p>
+          <Link to="/#contact">
+            <ContactUsIcon className={styles.linkIcon} />
+            {i18n.t("contact_us")}
+          </Link>
+        </section>
       </div>
       <div className={styles.horizontalDivider}>
         <hr />
       </div>
       <section className={styles.sectionContainer}>
         <h1>{i18n.t("Founders")}</h1>
-        <div className={styles.foundersAvatarList}>
+        <div
+          ref={founderRef}
+          className={`${styles.foundersAvatarList} ${styles[slideIn]}`}
+        >
           <div className={styles.founderContainer}>
             <Box
               bgColor="rgb(178, 34, 34,0.5)"
