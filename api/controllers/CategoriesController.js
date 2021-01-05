55b420561ca0e0d5b54c27fa685712e7fbdcd2bf
Sudarshan K J <kjsudi@gmail.com>
2021-01-05 07:05:30 -0800

    Filter on category now available. Only specific categories can be fetched too

--- api/controllers/CategoriesController.js
+++ api/controllers/CategoriesController.js
@@ -16,26 +16,37 @@ exports.health = (req, res) =>
     );
 
 exports.getCategories = (req, res) => {
-  const dirPath = path.join(__dirname, "../public/images/categories");
+  let dirPath = path.join(__dirname, "../public/images/categories");
+  if (req.query.categoryType) {
+    dirPath = path.join(
+      __dirname,
+      `../public/images/categories/${req.query.categoryType}`
+    );
+  }
 
   function getFiles(dir, parentPath) {
-    const dirents = fs.readdirSync(dir, { withFileTypes: true });
-    const categories = dirents.map((dirent) => {
-      const res = path.resolve(dir, dirent.name);
-      const category = {
-        key: dirent.name,
-        basePath: parentPath + "/" + dirent.name,
-        sub: [],
-        isFile: false,
-      };
-      if (dirent.isDirectory()) {
-        category.sub.push(...getFiles(res, category.basePath));
-      } else {
-        category.isFile = true;
-      }
-      return category;
-    });
-    return categories;
+    try {
+      const dirents = fs.readdirSync(dir, { withFileTypes: true });
+      const categories = dirents.map((dirent) => {
+        const res = path.resolve(dir, dirent.name);
+        const category = {
+          key: dirent.name,
+          basePath: parentPath + "/" + dirent.name,
+          sub: [],
+          isFile: false,
+        };
+        if (dirent.isDirectory()) {
+          category.sub.push(...getFiles(res, category.basePath));
+        } else {
+          category.isFile = true;
+        }
+        return category;
+      });
+      return categories;
+    } catch (err) {
+      logger.error("Error while reading directories", err);
+      return [];
+    }
   }
 
   const categories = getFiles(dirPath, "/static/images/categories");
