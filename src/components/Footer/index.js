bf6c1cfbc7bb7769ffd6c659223458f540ab6546
KJ Sudarshan <kjsudi@gmail.com>
2022-04-20 20:13:14 +0530

    update: year

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import styles from "./index.module.css";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>&copy; 2020 All rights reserved</p>
+      <p>&copy; 2022 All rights reserved</p>
       <p className={styles.credits}>
         Designed and Developed by
         <a href="https://krankj.in" target="_blank" rel="noopener noreferrer">
